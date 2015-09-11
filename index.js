c24d4f23d8fa450b4bf1a349e7573d63783e25cc
wanderer <mjbecze@gmail.com>
2015-09-11 16:56:37 +0000

    no more chrashing

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const Blockchain = require('ethereumjs-blockchain')
 const Block = require('ethereumjs-block')
 const Network = require('devp2p')
 const genesisHash = require('ethereum-common').genesisHash.v.slice(2)
-
 const EthPeerManager = require('./ethPeerManagner.js')
 const SyncManager = require('./syncManager.js')
 
@@ -40,14 +39,15 @@ network.on('connection', function (peer) {
 
   var peerMan = new EthPeerManager(stream)
   peerMan.on('status', function (status) {
+    console.log('status');
     //dissconect if wrong genesis
     if (genesisHash !== status.genesisHash.toString('hex')) {
       peer.sendDisconnect(peer.DISCONNECT_REASONS.SUBPROTOCOL_REASON)
     } else {
       console.log('td: ' + status.td.toString('hex'));
-      // syncManager.sync(headDetails.td, head.header.number, peerMan, function(){
-      //   console.log('done syncing'); 
-      // })
+      syncManager.sync(headDetails.td, head.header.number, peerMan, function(){
+        console.log('done syncing'); 
+      })
     }
   })
 
@@ -74,15 +74,12 @@ network.on('connection', function (peer) {
       })
     }
   ], function () {
-    console.log(headDetails.td);
-    var a = new Buffer(new BN(headDetails.td).toArray())
-    console.log(a.toString('hex'));
-    peerMan.sendStatus(new Buffer([01]), new Buffer('08a19410e94fa573', 'hex'), head.hash(), new Buffer(genesisHash, 'hex'))
+    var td = new Buffer(new BN(headDetails.td).toArray())
+    peerMan.sendStatus(new Buffer([01]), td, head.hash(), new Buffer(genesisHash, 'hex'))
   })
 })
 
 async.series([
-
   function (done) {
     network.listen(port, '0.0.0.0', done)
   },
